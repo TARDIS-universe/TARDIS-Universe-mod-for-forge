e3ce3bc806f5b4e672c931b7ef27c3aa8c8723a6
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2024-06-23 13:50:40 -0600

    added a few other thing and a block with a mesh model >:))

--- src/main/java/net/mcreator/tumodforforge/init/TuModForForgeModBlocks.java
+++ src/main/java/net/mcreator/tumodforforge/init/TuModForForgeModBlocks.java
@@ -16,6 +16,7 @@ import net.mcreator.tumodforforge.TuModForForgeMod;
 public class TuModForForgeModBlocks {
 	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, TuModForForgeMod.MODID);
 	public static final RegistryObject<Block> TRENZALORE_PORTAL = REGISTRY.register("trenzalore_portal", () -> new TrenzalorePortalBlock());
+	public static final RegistryObject<Block> MESHBLOCKBUDS = REGISTRY.register("meshblockbuds", () -> new MeshblockbudsBlock());
 	// Start of user code block custom blocks
 	// End of user code block custom blocks
 }
--- src/main/java/net/mcreator/tumodforforge/init/TuModForForgeModItems.java
+++ src/main/java/net/mcreator/tumodforforge/init/TuModForForgeModItems.java
@@ -8,7 +8,9 @@ import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
 
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.BlockItem;
 
 import net.mcreator.tumodforforge.item.TrenzaloreItem;
 import net.mcreator.tumodforforge.TuModForForgeMod;
@@ -16,6 +18,11 @@ import net.mcreator.tumodforforge.TuModForForgeMod;
 public class TuModForForgeModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, TuModForForgeMod.MODID);
 	public static final RegistryObject<Item> TRENZALORE = REGISTRY.register("trenzalore", () -> new TrenzaloreItem());
+	public static final RegistryObject<Item> MESHBLOCKBUDS = block(TuModForForgeModBlocks.MESHBLOCKBUDS);
+
 	// Start of user code block custom items
 	// End of user code block custom items
+	private static RegistryObject<Item> block(RegistryObject<Block> block) {
+		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties()));
+	}
 }
